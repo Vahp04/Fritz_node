384da868f089ccd4bc4ba1e35755a99c5f2c4f67
Vahp04 <valeriaahp09@gmail.com>
2025-11-25 16:10:11 -0400

    Intento de solucionar los reportes 15.4.5

--- src/services/PDFKitGenerator.js
+++ src/services/PDFKitGenerator.js
@@ -124,7 +124,7 @@ class PDFKitGenerator {
 
       // Encabezados de la tabla
       const headers = ['Usuario', 'Cargo', 'Correo', 'RDP', 'Sede', 'Depto', 'Total', 'Activos', 'Estado'];
-      const colWidths = [73, 48, 113, 68, 50, 43, 25, 25, 50];
+      const colWidths = [73, 48, 115, 69, 50, 43, 25, 25, 50];
       
       // Fondo rojo para encabezados
       doc.rect(margin, yPosition, pageWidth, 15)
