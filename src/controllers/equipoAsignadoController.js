65c059da5090aa110cfacab9a70ddae9ae28abd0
Vahp04 <valeriaahp09@gmail.com>
2025-11-26 11:16:32 -0400

    Intento de solucionar los reportes 16.4.5

--- src/controllers/equipoAsignadoController.js
+++ src/controllers/equipoAsignadoController.js
@@ -1469,7 +1469,7 @@ async verPdfAsignaciones(req, res) {
         if (data.equiposAsignados.length > 0) {
             // Encabezados de tabla
             const headers = ['Usuario', 'Cargo', 'Sede', 'Departamento', 'Equipo', 'Tipo', 'Serial', 'Fecha Asig.', 'Fecha Dev.', 'IP Equipo', 'Asignado Por', 'Estado'];
-            const columnWidths = [70, 57, 58, 70, 100, 57, 60, 58, 60, 60, 67, 60];
+            const columnWidths = [67, 57, 58, 70, 98, 55, 60, 58, 60, 60, 67, 60];
             
             let headerX = 20;
             
